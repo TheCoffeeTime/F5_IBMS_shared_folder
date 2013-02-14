d4b094c9b569fc01f114a01db63e457ee1bbbd38
Thanakorn Tuanwachat <oak.thanakorn@gmail.com>
2013-02-14 21:22:49 +0000

    Initial setup.
    First template on GitHub.

---
+++ F5_IMBS_shared_folder/working_folder/Main.java
@@ -0,0 +1,28 @@
+/*
+ * A very simple application illustrating how to use the interface.
+ * Prints the names of all the drivers in the database.
+ * @author John Sargeant
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //Connect to the database
+        database.openBusDatabase();
+        //-------------------------------------------------------------------
+        //Body
+        
+        //Get all the id of drivers in the database
+        int[] driverIDs = DriverInfo.getDrivers();
+        
+        //For each of those ID, get the name and print them
+        for (int i=0; i<driverIDs.length; i++)
+            System.out.println(DriverInfo.getName(driverIDs[i]));
+        
+        //-------------------------------------------------------------------
+    }
+
+}
