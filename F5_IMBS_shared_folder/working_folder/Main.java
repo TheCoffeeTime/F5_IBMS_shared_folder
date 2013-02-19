8ddf0043271860723c842a437b5a6f2fc622f9fd
Nathan <nathantilsley@btinternet.com>
2013-02-19 10:19:00 +0000

    calculate interval between two dates

--- F5_IMBS_shared_folder/working_folder/Main.java
+++ F5_IMBS_shared_folder/working_folder/Main.java
@@ -3,6 +3,7 @@
  * Prints the names of all the drivers in the database.
  * @author John Sargeant
  */
+import java.util.Date;
 public class Main {
 
     /**
@@ -21,6 +22,12 @@ public class Main {
         //For each of those ID, get the name and print them
         for (int i=0; i<driverIDs.length; i++)
             System.out.println(DriverInfo.getName(driverIDs[i]));
+        
+        Date date1 = new Date(2011, 12, 31);
+        Date date2 = new Date(2011, 12, 31);
+     
+        int daysBetween = Dates.calculateInterval(date1, date2);
+        System.out.println(daysBetween);     
         
         //-------------------------------------------------------------------
     }
